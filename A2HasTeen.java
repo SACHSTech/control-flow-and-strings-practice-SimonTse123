c2ed49318af80a27fcfa6fff8eba3a727d8754c3
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-04-01 13:43:18 -0400

    modified:   A2HasTeen.java

--- A2HasTeen.java
+++ A2HasTeen.java
@@ -1,1 +1,33 @@
+class A2HasTeen extends ConsoleProgram {
+    
+    /**
+     * A program A2HasTeen.java that will ouput true when at least one of the 3 inputted values are between 13 and 19
+     * @author: Simon Tse
+     */
 
+     public void run() {
+        // Declare variables
+        int intAgeOne;
+        int intAgeTwo;
+        int intAgeThree;
+
+        // Get variables
+        intAgeOne = readInt("Enter the age of the first person: ");
+        intAgeTwo = readInt("Enter the age of the second person: ");
+        intAgeThree = readInt("Enter the age of the third person: ");
+
+        // Check whether values fulfill requirements and show results
+        if (13 <= intAgeOne || intAgeOne <= 19){
+            System.out.println("True");
+            if (13 <= intAgeTwo || intAgeThree <= 19){
+                System.out.println("True");
+                if (13 <= intAgeThree || intAgeThree <= 19){
+                    System.out.println("True");
+                }
+            }
+        }
+        else {
+            System.out.println("False");
+        }
+     }
+}
